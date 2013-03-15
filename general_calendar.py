fedc7bbdc27e916f0d04f6185873e78b79cef5bb
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-03-15 16:02:40 +0100

    Start inputing data from the General Roman Calendar (in Italian, so far).

---
+++ general_calendar.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+TYPE_S = 0
+TYPE_F = 1
+TYPE_M = 2
+TYPE_FM = 3
+
+GENERAL_CALENDAR = [
+    (1, 1, u"Maria SS. Madre di Dio", TYPE_S),
+    (1, 2, u"Ss. Basilio Magno e Gregorio Nazianzeno, vescovi e dottori della Chiesa", TYPE_M),
+    (1, 6, u"Epifania del Signore", TYPE_S),
+    (1, 7, u"S. Raimondo da Peñafort", TYPE_FM),
+    (1, 13, u"S. Ilario, vescovo e dottore della Chiesa", TYPE_FM),
+    (1, 17, u"S. Antonio, abate", TYPE_M),
+    (1, 20, u"S. Fabiano, papa e martire", TYPE_FM),
+    (1, 20, u"S. Sebastiano, martire", TYPE_FM),
+    (1, 21, u"S. Agnese, vergine e martire", TYPE_M),
+    (1, 22, u"S. Vincenzo, diacono e martire", TYPE_FM),
+    (1, 24, u"S. Francesco di Sales, vescovo e dottore della Chiesa", TYPE_M),
+    (1, 25, u"Conversione di S. Paolo, apostolo", TYPE_F),
+    (1, 26, u"Ss. Timòteo e Tito, vescovi", TYPE_M),
+    (1, 27, u"S. Angela Merici, vergine", TYPE_FM),
+    (1, 28, u"S. Tommaso d'Aquino, sacerdote e dottore della Chiesa", TYPE_M),
+    (1, 31, u"S. Giovanni Bosco, sacerdote", TYPE_M),
+
+    (2, 2, u"Presentazione del Signore", TYPE_F),
+    (2, 3, u"S. Biagio, vescovo e martire", TYPE_FM),
+    (2, 4, u"S. Ansgario (Oscar), vescovo", TYPE_FM),
+    (2, 5, u"S. Agata, vergine e martire", TYPE_M),
+    (2, 6, u"Ss. Paolo Miki e compagni, martiri", TYPE_M),
+    (2, 8, u"S. Girolamo Emiliani", TYPE_FM),
+    (2, 8, u"S. Giuseppina Bakhita, vergine", TYPE_FM),
+    (2, 10, u"S. Scolastica, vergine", TYPE_M),
+    (2, 11, u"Beata Vergine Maria di Lourdes", TYPE_FM),
+    (2, 14, u"Ss. Cirillo, monaco, e Metodio, vescovo, patroni d'Europa", TYPE_F),
+    (2, 17, u"Ss. Sette Fondatori dell'Ordine dei Servi della beata Vergine Maria", TYPE_FM),
+    (2, 21, u"S. Pier Damiani, vescovo e dottore della Chiesa", TYPE_FM),
+    (2, 22, u"Cattedra di S. Pietro, apostolo", TYPE_F),
+    (2, 23, u"S. Policarpo, vescovo e martire", TYPE_M),
+
+    (3, 4, u"S. Casimiro", TYPE_FM),
+    (3, 7, u"Ss. Perpetua e Felicita, martiri", TYPE_M),
+    (3, 8, u"S. Giovanni di Dio, religioso", TYPE_FM),
+    (3, 9, u"S. Francesca Romana, religiosa", TYPE_FM),
+    (3, 17, u"S. Patrizio, vescovo", TYPE_FM),
+    (3, 18, u"S. Cirillo di Gerusalemme, vescovo e dottore della Chiesa", TYPE_FM),
+    (3, 19, u"S. Giuseppe, sposo della beata Vergine Maria", TYPE_S),
+    (3, 23, u"S. Turibio de Mongrovejo, vescovo", TYPE_FM),
+    (3, 25, u"Annunciazione del Signore", TYPE_S),
+
+    (4, 2, u"S. Francesco de Paola, eremita", TYPE_FM),
+    (4, 4, u"S. Isidoro, vescovo e dottore della Chiesa", TYPE_FM),
+    (4, 5, u"S. Vincenzo Ferrer, sacerdone", TYPE_FM),
+]
