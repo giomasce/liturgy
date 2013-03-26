c4e7029e69bc7685d60a705f247bb59569aff683
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-03-26 13:14:41 +0100

    Start designing the database.

---
+++ database.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, backref
+
+db = create_engine('sqlite:///prova.sqlite', echo=True)
+Session = sessionmaker(db)
+Base = declarative_base(db)
+
+class Event(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
+    priority = Column(Integer, nullable=False)
+
+    __mapper_args__ = {'polymorphic_on': type}
+
+class FixedEvent(Event):
+    __mapper_args__ = {'polymorphic_identity': 'fixed'}
+
+    day = Column(Integer, nullable=False)
+    month = Column(Integer, nullable=False)
+
+class TimedEvent(Event):
+    __mapper_args__ = {'polymorphic_identity': 'timed'}
+
+    weekday = Column(Integer, nullable=False)
+    week = Column(Integer, nullable=False)
+    season = Column(Integer, nullable=False)
+
+class MovableEvent(Event):
+    __mapper_args__ = {'polymorphic_identity': 'movable'}
+
+    calc_func = Column(String, nullable=False)
+
+class Mass(Base):
+    __tablename__ = 'masses'
+
+    id = Column(Integer, primary_key=True)
+    order = Column(Integer, nullable=False)
+    event_id = Column(Integer, ForeignKey(Event.id), nullable=False)
+    digit = Column(String, nullable=True)
+    letter = Column(Integer, nullable=True)
+    title = Column(String, nullable=True)
+
+    event = relationship(Event,
+                         backref=backref('masses', order_by=order))
+
+class Reading(Base):
+    __tablename__ = 'readings'
+
+    id = Column(Integer, primary_key=True)
+    order = Column(Integer, nullable=False) 
+    mass_id = Column(Integer, ForeignKey(Mass.id), nullable=False)
+    quote = Column(String, nullable=True)
+    text = Column(String, nullable=True)
+    status = Column(String, nullable=False, default='unknown')
+    title = Column(String, nullable=False)
+
+    mass = relationship(Mass,
+                        backref=backref('readings', order_by=order))
+
+if __name__ == '__main__':
+    Base.metadata.create_all()
