4b78a290dd7bde9e04e9fe97883a97391b7d501e
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-04-27 00:00:48 +0200

    Some small usability fixes.

--- edit_reading.py
+++ edit_reading.py
@@ -7,7 +7,7 @@ import tempfile
 import codecs
 
 from database import Session, Reading
-from quote import BibleQuery, decode_quote
+from quote import BibleQuery, decode_quote, convert_quote_psalm_numbering
 from utils import PrependStream
 
 def main():
@@ -16,17 +16,22 @@ def main():
     bible_query = BibleQuery()
     reading = session.query(Reading).filter(Reading.id == reading_id).one()
 
-    temp, tempname = tempfile.mkstemp()
+    temp, tempname = tempfile.mkstemp(suffix='.txt')
     temp = os.fdopen(temp, 'w')
+    # From http://stackoverflow.com/questions/15120346/emacs-setting-comment-character-by-file-extension
+    PrependStream(temp, '# ').write('-*- comment-start: "#"; -*-\n')
+    temp.write('\n')
     temp.write(reading.text.encode('utf-8'))
+    temp.write('\n')
     try:
-        bible_text = bible_query.get_text(decode_quote(reading.quote, allow_only_chap=True))
+        converted_quote = convert_quote_psalm_numbering(reading.quote, False)
+        bible_text = bible_query.get_text(decode_quote(converted_quote, allow_only_chap=True))
     except:
-        PrependStream(temp, '# ').write('\nQuote: %s\nCould not retrieve bible text\n' % (reading.quote.encode('utf-8')))
+        PrependStream(temp, '# ').write('Quote: %s\nCould not retrieve bible text\n' % (reading.quote.encode('utf-8')))
         print decode_quote(reading.quote, allow_only_chap=True)
         raise
     else:
-        PrependStream(temp, '# ').write('\nQuote: %s\nBible text:\n' % (reading.quote.encode('utf-8')) + bible_text.encode('utf-8'))
+        PrependStream(temp, '# ').write('Quote: %s\nConverted quote: %s\nBible text:\n\n' % (reading.quote.encode('utf-8'), converted_quote.encode('utf-8')) + bible_text.encode('utf-8'))
     temp.close()
 
     os.system("emacs -nw %s" % (tempname))
@@ -35,6 +40,7 @@ def main():
         for line in fin:
             if not line.startswith('#'):
                 new_text += line
+    new_text = new_text.strip() + u'\n'
 
     if new_text == reading.text:
         sys.stdout.write("Text wasn't modified, ignoring...\n")
