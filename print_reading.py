d1e0ff0e4224ee26f33407e5197bdf2f8d3afc77
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-12-14 19:48:00 +0100

    Minor.

--- print_reading.py
+++ print_reading.py
@@ -5,13 +5,15 @@ import sys
 import re
 
 from database import Session, Reading
+from utils import PrependStream
 
 def main():
     reading_id = int(sys.argv[1])
     session = Session()
     reading = session.query(Reading).filter(Reading.id == reading_id).one()
     text = reading.text
-    sys.stdout.write(text)
+    PrependStream(sys.stdout, '    ').write('"' + text.strip() + '"')
+    sys.stdout.write('\n')
     session.rollback()
 
 if __name__ == '__main__':
